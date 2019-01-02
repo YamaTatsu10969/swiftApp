5164004d1926417ff6777594ff3d6bd15fa36253
山本竜也 <yamamototatsuya@macbook-pro.local>
2019-01-02 18:40:30 +0900

    Initial Commit

---
+++ Amazon.co.jp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amazon.co.jp
+//
+//  Created by 山本竜也 on 2019/1/2.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
