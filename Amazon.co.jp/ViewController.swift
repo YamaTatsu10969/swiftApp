26a0d6d08a9e5f2d0bcf852a8b38b502a8d07ec6
山本竜也 <yamamototatsuya@MacBook-Pro.local>
2019-01-03 23:38:23 +0900

    before submit

--- Amazon.co.jp/ViewController.swift
+++ Amazon.co.jp/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UISearchBarDelegate {
+    
+  
+    @IBOutlet weak var myToolBar: UIToolbar!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
-
+    
+    
 }
+
+    extension UISearchBar {
+        func disableBlur() {
+            backgroundImage = UIImage()
+            isTranslucent = true
+        }
+    }
 
